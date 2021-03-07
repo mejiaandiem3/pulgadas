0be1036789b8e8e1ee616b153d27944e2ef2dc45
Andrea Evelyn Mejía Rubio <andiemartin33@gmail.com>
2021-03-07 02:11:39 -0600

    Agregar archivos de proyecto.

---
+++ pulgadas/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pulgadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double pulgada=2.54; //1 pulgada = 2.54 cm 
+            double centimetrosconv;
+            double solucion1;
+
+            Console.WriteLine("Conversion de centimetros a pulgadas.");
+
+            Console.WriteLine("Escribe los centimetros a convertir: ");
+            centimetrosconv = Convert.ToDouble(Console.ReadLine());
+
+            solucion1 = centimetrosconv * pulgada;
+
+            Console.WriteLine("Este es el valor en pulgadas: "+ solucion1);
+
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
